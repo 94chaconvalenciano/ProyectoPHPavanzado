e6c1706d566d0f4fe5bb711836f844954598f921
Carolina De La Ossa <42593603+cdelaossa91@users.noreply.github.com>
2019-02-13 23:09:22 -0600

    Cambios Finales
    Agregue mas cosas al txt y ademas hice un intento frustrado de agregar un XML y un json, igual dejo el codigo de referencia

--- puntarenas.php
+++ puntarenas.php
@@ -22,14 +22,17 @@
 		<p>Está situada en el sur de Costa Rica. La conforman los dos cantones productores de café más jóvenes: Coto Brus, con frontera con Panamá, y Pérez Zeledón.</p>
 		<p>Coto Brus se ubica en las faldas de la Cordillera de Talamanca, que divide Costa Rica con respecto a los océanos Pacifico y Atlántico. También Pérez Zeledón, pero más al noroeste cerca del pico más alto de Costa Rica, el Chirripó (3.820 mts.)</p>
 		<p id="texto"></p>
+        <h3 id="cotobrus">Coto Brus</h3>
+		<p id="texto2"></p>
+		<h3 id="perez">Perez Zeledon</h3>
+		<p id="texto3"></p>
 		
      <!--    ********* a. Coto Brus y b. Perez Zeledon ******-->	   
-        <div id="regiones">
-    
-		</div>
+        <div id="region"></div>
+		
 		<button onclick="mostrar()" id="vermas">Mostrar Mas</button>
 		<button onclick="ocultar()" id="vermenos">Mostrar Menos</button>
-		<button onclick="regiones()" id="region">Regiones</button>
+		<!--<button onclick="regiones()" id="region">Regiones</button>-->
 		
 
 	</div>
@@ -57,43 +60,86 @@
 	<script type="text/javascript">
         /*----------- 3. Ajax de Region Cafetalera Brunca ---------*/
 		$("#vermenos").hide();
-        $("#regiones").hide();
+        $("#cotobrus").hide();
+        $("#perez").hide();
+     
 		function mostrar(){
 			$("#texto").show();
-			$("#texto").load("assets/puntarenas.txt");
+            $("#cotobrus").show();
+            $("#perez").show();
+             $("#texto2").show();
+              $("#texto3").show();
+            $("#texto").load("assets/puntarenas.txt");
+            $("#texto2").load("assets/cotobrus.txt");
+             $("#texto3").load("assets/perez.txt");
 			$("#vermas").hide();
 			$("#vermenos").show();
 		}
 
 		function ocultar(){
 			$("#texto").hide();
+            $("#cotobrus").hide();
+            $("#texto2").hide();
+            $("#perez").hide();
+            $("#texto3").hide();
 			$("#vermenos").hide();
 			$("#vermas").show();
 		}
 
+        // Regiones con XML
+	
+      
+        
+        
+        
+        
+        
+        
+        /*
+        function leer(url, callback) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    var data = JSON.parse(xmlhttp.responseText);
+                    callback(data);
+                }
+            };
 
-		function regiones(){
-            $('#regiones').show();
-            
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+            }
+
+
+
+        leer('assets/brunca.json', function(data) {
+
+            var html = "<h2>" + data["region1"] + "</h2>";
+            html += "<p>" + data["descripcion1"] + "</p>";
+            html += "<ul class='list-group'>";
+            for (var i=0; i < data["caracteristicas1"].length; i++) { html +="<li>" + data["caracteristicas1"][i]["titulo"] + "</li>" ; } html +="</ul>" ; document.getElementById("region").innerHTML= html; 
+        });
+        
+        
+        
+        
+        */
+        
+        
+        
+        
+        /*	function regiones(){
 			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function(){
+			xhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200){
-					var contenido = xhr.responseText;
                      leer(this);
-					//Parte de Galeria de Imagenes
-                    var imagen = new Image();
-					imagen.src = contenido;
-                   
+        
 				}
 			};
             
 			xhttp.open("GET", "assets/brunca.xml", true);
             xhttp.send();
-          
-            //Parte de Galeria de Imagenes
-            xhttp.open("GET",urlImagen,true);
-			xhttp.send(null);
 		}
+        
         
         function leer(xml){
             var i;
@@ -111,11 +157,32 @@
                 
             }
             
-            document.getElementById("#regiones").innerHTML = lista;
-        }
+            document.getElementById("region").innerHTML = lista;
+        }*/
+        
+        
+        
+        
+        
+        
 
          /*----------- 4. Ajax de Galeria de Imagenes ---------*/
-         function aumentar(imagen){
+         
+        function precarga(url){
+            var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function(){
+				if (this.readyState == 4 && this.status == 200){
+					var contenido = xhttp.responseText;
+                    var imagen = new Image();
+					imagen.src = contenido;
+                }
+            };
+            
+            xhttp.open("GET",urlImagen,true);
+			xhttp.send(null);
+        }
+        
+        function aumentar(imagen){
             var directorio = imagen.src;
             var imagenAumentada = "<img src='" + directorio + "'>";
             document.getElementById("imagenGrande").innerHTML = imagenAumentada;
